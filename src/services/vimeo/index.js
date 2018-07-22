65d552c5863ddfdd631b6fa83b73ae03c1ffb638
Misha Milovidov <mmilovidov@doterra.com>
2018-07-22 15:38:05 -0600

    Add creds for vimeo and wordpress services

---
+++ src/services/vimeo/index.js
@@ -0,0 +1,4 @@
+export const CLIENT_IDENTIFIER = '07edb52961118d48e9d18d3985365cd463a18356';
+export const CLIENT_SECRET = 'ZAw6lbGJ0bGWiy58AR8p+oCcR2yVdvLfzzcm5ig+03wFu4Yakhon8HeuoZdd13P8KSvo7J4B+iUIXRCzPumpiUwbIu61ijYd99md3PcIAFvmfJltm7iA7HdMK4V7pyxY';
+export const ACCESS_TOKEN = '9a8b245809b7f044938c36e863d706e1'; // scopes: public private create edit delete interact
+export const ROOT_URL = `https://api.vimeo.com`;
---
+++ src/services/wordpress/index.js
@@ -0,0 +1,3 @@
+export const API_TOKEN = 'm4$7TzRMcr1(4EUmJT&^X3j3ncyMLK@oCtoVseN9Drt(ykusB)uYbvcOKe17k5dK';
+export const SITE_ID = '149357280';
+export const ROOT_URL = `https://public-api.wordpress.com/rest/v1.1/sites/${SITE_ID}`;

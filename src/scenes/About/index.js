23f6b889dc3d1972e1b4a0da5e01a758f2dded6d
Misha Milovidov <mmilovidov@doterra.com>
2018-08-12 15:21:40 -0600

    Finish about me component

--- src/scenes/About/index.js
+++ src/scenes/About/index.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getBlogPost } from '../../services/wordpress/actions';
 import './styles.css';
 
 class About extends Component {
+  componentDidMount() {
+    this.props.getBlogPost('about-me');
+  }
+
   render() {
+    const { blogPost, hasErrored, isLoading } = this.props;
+    const blogFeatureImage = blogPost.featured_image;
+    const blogTitle = blogPost.title;
+    const blogContent = blogPost.content;
+
+    if (isLoading || hasErrored) {
+      return (
+        <div className="About"></div>
+      );
+    }
+
     return (
       <div className="About">
-        About
+        <img
+            src={blogFeatureImage}
+            alt={blogTitle}
+            onError={(e)=>{
+              e.target.src="//dummyimage.com/400x400/e5e5e5/e5e5e5.png"
+            }}
+          />
+        <div className="content" dangerouslySetInnerHTML={{__html: blogContent}} />
       </div>
     );
   }
 }
 
-export default About;
+function mapStateToProps(state, ownProps) {
+  return {
+    blogPost: state.blogPost,
+    hasErrored: state.blogPostFetchError,
+    isLoading: state.blogPostIsLoading
+  };
+}
+
+export default connect(mapStateToProps, { getBlogPost })(About);
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { GalleryReducer } from './scenes/Gallery/reducer';
+import { BlogPostReducer } from './services/wordpress/reducer';
 
 const rootReducer = combineReducers({
-  ...GalleryReducer
+  ...GalleryReducer,
+  ...BlogPostReducer
 });
 
 export default rootReducer;
